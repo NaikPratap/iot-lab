ace7323f2918b6a1f7908e2aa97a543fd5abd085
NaikPratap <pratapnaik605@gmail.com>
2025-08-13 15:23:41 +0530

    Add files via upload

---
+++ lab1b.ino
@@ -0,0 +1,21 @@
+// C++ code
+//
+const int led[]={2,3,4,5,6};
+void setup()
+{
+  for (int i=4;i>=0;i=i++)
+       {
+         pinMode(led[i], OUTPUT);
+   
+  }
+}
+void loop()
+{
+  for(int i=4;i>=0;i=i++){
+  
+  digitalWrite(led[i], HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(led[i], LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+}
+}
